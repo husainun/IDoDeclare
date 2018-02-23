5aead28e41c45675a035ccd4f481477a36e41ce5
Uzma <uzmanhusain@yahoo.com>
2018-02-23 13:34:50 -0600

    First commit

---
+++ Declarejs.js
@@ -0,0 +1,20 @@
+
+ var num1 =  0;
+ var num2 = 2;
+ var num3 = - 56;
+ var num4 = 0.3;
+var string1 = 'Hello';
+var string2 =  "Coding";
+var string3 = "Dojo People";
+var bool1=  'True';
+var bool2 =  "False";
+var notSure;
+
+console.log("Here is my greeting:", string1);
+console.log("to my ", string2,  string3);
+console.log("The first number is " ,num1);
+console.log("The second number is " ,num2);
+console.log("The third number is " + num3);
+console.log("The fourth number is " + num4);
+console.log("Do I love cofee ? " + bool1 + " or " + bool2);
+console.log("What am I", notSure);
